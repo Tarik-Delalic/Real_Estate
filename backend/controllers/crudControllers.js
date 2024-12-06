9a42f885ffda09598f58693d25d78e7c31921449
Tarik Delalic <48967705+dela777@users.noreply.github.com>
2024-12-06 23:07:27 +0100

    Deleted files
    De

--- backend/controllers/crudControllers.js
+++
@@ -1,5 +0,0 @@
-const getRequest = (req, res, next) => {
-    res.status(200).json({ message: 'Successfully connected to backend!!!' })
-};
-
-module.exports = { getRequest };
--- backend/views/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const { getRequest } = require('../controllers/crudControllers');
-
-
-routes.get('/admin', getRequest);
-
-
-module.exports = routes;
